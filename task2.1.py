79bc3c99fc25a03fa4d02ec3a6a4c4e48d0eece1
JuliiaOrishchenko <47636150+JuliiaOrishchenko@users.noreply.github.com>
2022-09-18 00:12:56 +0300

    Initial commit

---
+++ task2.1.py
@@ -0,0 +1,6 @@
+name = 'Yuliia Orishchenko'
+day = 'Monday'
+
+print(f'Good day {name}! {day} is a perfect day to learn some python')
+print('Good day {}! {} is a perfect day to learn some python'.format(name, day))
+print('Good day %s! %s is a perfect day to learn some python' % (name, day))
---
+++ task2.2.py
@@ -0,0 +1,3 @@
+first_name = 'Yuliia'
+last_name = 'Orishchenko'
+print('Hello' + ' ' + first_name + ' ' + last_name + '!')
